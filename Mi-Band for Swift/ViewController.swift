a7598251c950800d0643af37d1727a8b4a9cd96e
Daniel Weber <danielweber@daniels-macbook-pro.local>
2017-07-29 20:31:57 +0200

    Initial Commit

---
+++ Mi-Band for Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mi-Band for Swift
+//
+//  Created by Daniel Weber on 29.07.17.
+//  Copyright © 2017 zero2one. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
